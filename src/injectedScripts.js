2be777ba5c78c050b58517000bef3d52d637e604
root <root@ip-172-31-28-218.ec2.internal>
2024-11-11 10:19:53 +0000

    Auto-commit:-Injected-new-script-and-ready-for-deployment970

--- src/injectedScripts.js
+++ src/injectedScripts.js
@@ -2,11 +2,4 @@
 export const injectedScript = () => {
 // No injected script yet
   alert("Hello! I am an alert box!!");
-  alert("Hello! I am an alert box again!!");
-  alert("Hello! I am an alert box third!!");
-  alert("Hello! I am an alert box third!!");
-  alert("Hello! I am an alert box!!");
-  alert("Hello! I am an alert finaaaaaaaal!!");
-  console.log("Hello world!");
-
 };
