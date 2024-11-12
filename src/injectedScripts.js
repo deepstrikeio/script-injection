68dbe1c15ae0ea07ee323328544a670cd11a3cbb
muhammad-maged-ds <muhammadmaged@Muhammads-MacBook-Pro.local>
2024-11-12 15:09:30 +0200

    Remove comment

--- src/injectedScripts.js
+++ src/injectedScripts.js
@@ -1,10 +1,4 @@
 // src/injectedScripts.js
 export const injectedScript = () => {
 // No injected script yet
-  alert("Hello! I am an alert box!!");
-  alert("Hello! I am an alert second time");
-  alert("Hello! I am an alert box!!");
-  alert("Hello! I am an alert box!!");
-  alert("Hello! I am an alert final!!");
-  alert("cron wotks");
 };
