e4c384f203e039527d3cf94d7900661166b69d8f
muhammad-maged-ds <muhammadmaged@Muhammads-MacBook-Pro.local>
2024-11-14 13:12:33 +0200

    clean

--- src/injectedScripts.js
+++ src/injectedScripts.js
@@ -1,11 +1,2 @@
 export const injectedScript = () => {
-export const injectedScript = () => {
-// src/injectedScripts.js
-export const injectedScript = () => {
-// No injected script yet
-};
-  alert("Hello! I am an alert box!!");
-
-};
-  /* hello this is normal txt */
 };
