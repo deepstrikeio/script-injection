e3e5112003f3ecaae6a12e9e5eba63cdd3f965f3
root <root@ip-172-31-28-218.ec2.internal>
2024-11-14 11:08:17 +0000

    Auto-commit:-Injected-new-script-and-ready-for-deployment122

--- src/injectedScripts.js
+++ src/injectedScripts.js
@@ -1,4 +1,11 @@
+export const injectedScript = () => {
+export const injectedScript = () => {
 // src/injectedScripts.js
 export const injectedScript = () => {
 // No injected script yet
+};
+  alert("Hello! I am an alert box!!");
+
+};
+  /* hello this is normal txt */
 };
